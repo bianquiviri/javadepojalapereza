b01f446a902f81470f9f5f503f8a43a01ca16767
Jose Antonio Bianco Paredes <bianquiviri@gmail.com>
2023-03-28 02:40:20 -0500

    Ya fix al proyect

--- src/main/java/com/platzi/market/MarketApplication.java
+++ src/main/java/com/platzi/market/MarketApplication.java
@@ -1,13 +1,13 @@
-package com.platzimarket;D
+package com.platzi.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlatziMarketApplication {
+public class MarketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlatziMarketApplication.class, args);
+		SpringApplication.run(MarketApplication.class, args);
 	}
 
 }
--- src/test/java/com/platzi/market/MarketApplicationTests.java
+++ src/test/java/com/platzi/market/MarketApplicationTests.java
@@ -1,10 +1,10 @@
-package com.platzimarket;
+package com.platzi.market;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PlatziMarketApplicationTests {
+class MarketApplicationTests {
 
 	@Test
 	void contextLoads() {
